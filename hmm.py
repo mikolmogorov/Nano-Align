e0f9441c64491727284d6a030ead0fae64ce85f5
Mikhail Kolmogorov <fenderglass@gmail.com>
2015-10-04 15:02:05 -0700

    no empty states iside

--- hmm.py
+++ hmm.py
@@ -5,6 +5,7 @@ import sys
 from string import maketrans
 from itertools import repeat, product
 from collections import namedtuple
+import math
 
 from statsmodels.nonparametric.smoothers_lowess import lowess
 import matplotlib.pyplot as plt
@@ -20,11 +21,12 @@ VolSigRegression = namedtuple("VolSigRegression", ["slope", "intercept", "std"])
 
 class NanoHMM(object):
     def __init__(self):
-        self.peptide = "ARTKQTARKSTGGKAPRKQL"
+        #self.peptide = "ARTKQTARKSTGGKAPRKQL"
+        self.peptide = "ASVATELRCQCLQTLQGIHPKNIQSVNVKSPGPHCAQTEVIATLKNGRKACLNPASPIVKKIIEKMLNSDKSN"
         self.window = 4
-        self.average = 30
-        self.flank = 50
-        self.reverse = True
+        self.average = 5
+        self.flank = 10
+        self.reverse = False
         self.regression = None
         self.init_distr = None
         self.trans_table = None
@@ -44,7 +46,7 @@ class NanoHMM(object):
             self.id_to_state[state_id] = state
 
     def set_init_distr(self):
-        self.init_distr = np.zeros(len(self.state_to_id))
+        self.init_distr = np.ones(len(self.state_to_id)) * 0.000001
         light_init = "-" * (self.window - 1) + "L"
         med_init = "-" * (self.window - 1) + "M"
         heavy_init = "-" * (self.window - 1) + "H"
@@ -52,19 +54,35 @@ class NanoHMM(object):
         self.init_distr[self.state_to_id[med_init]] = float(1) / 3
         self.init_distr[self.state_to_id[heavy_init]] = float(1) / 3
 
+        ending_states = [light_init[::-1], med_init[::-1], heavy_init[::-1]]
+        self.ending_states = map(self.state_to_id.get, ending_states)
+
+    def model_fit(self, inferred):
+        #for event in discrete_events:
+        #model = map(lambda e: self.regression.intercept +
+        #            self.regression.slope * e, self.pep_volumes)
+        inferred = map(lambda s: _avg_volume(self.id_to_state[s]), inferred)
+        plt.plot(inferred)
+        plt.plot(self.pep_volumes)
+        plt.show()
+
+
     def set_trans_table(self):
-        self.trans_table = np.zeros((len(self.state_to_id),
-                                     len(self.state_to_id)))
+        self.trans_table = np.ones((len(self.state_to_id),
+                                    len(self.state_to_id))) * 0.000001
         for st_1, st_2 in product(xrange(len(self.state_to_id)), repeat=2):
             seq_1 = self.id_to_state[st_1]
             seq_2 = self.id_to_state[st_2]
             if seq_1[1:] == seq_2[:-1]:
                 self.trans_table[st_1][st_2] = 0.25
 
-    def emission_prob(state_id, observation):
-        volume = _avg_volume(self.id_to_state(state_id))
+    def emission_prob(self, state_id, observation):
+        volume = _avg_volume(self.id_to_state[state_id])
         expec_mean = self.regression.intercept + self.regression.slope * volume
-        return norm(expec_mean, self.regression.std).cdf(observation)
+        x = norm(expec_mean, self.regression.std).pdf(observation)
+        #print(expec_mean, observation)
+        #print(x)
+        return x
 
     def learn_emissions_distr(self, events):
         volumes = []
@@ -80,6 +98,7 @@ class NanoHMM(object):
         for event in events:
             event = _normalize(event)
             discretized = []
+            self.pep_volumes = []
             #volumes = []
             #signals = []
             #xxx = []
@@ -95,6 +114,7 @@ class NanoHMM(object):
                 signal = np.mean(event[left:right])
 
                 volumes.append(volume)
+                self.pep_volumes.append(volume)
                 signals.append(signal)
                 discretized.append(signal)
                 #xxx.append(signal_pos)
@@ -114,40 +134,45 @@ class NanoHMM(object):
         self.regression = VolSigRegression(res[0], res[1], res[4])
         return discrete_events
 
-
     def init_state_distr():
         pass
 
     def hmm(self, observ_seq):
         num_observ = len(observ_seq)
-        num_states = len(init_distr)
+        num_states = len(self.init_distr)
         dp_mat = np.zeros((num_states, num_observ))
         backtrack = np.zeros((num_states, num_observ))
 
         for st in xrange(num_states):
-            dp_mat[st][0] = (math.log(init_distr[st]) +
-                             math.log(emission_prob(st, observ_seq[0])))
+            dp_mat[st][0] = (math.log(self.init_distr[st]) +
+                             math.log(self.emission_prob(st, observ_seq[0])))
             backtrack[st][0] = None
 
         #filling dp matrix
         for obs in xrange(1, num_observ):
             for st in xrange(num_states):
-                max_val = -float(sys.maxint)
+                if (self.id_to_state[st].count("-") and
+                    self.window <= obs < num_observ - self.window):
+                    dp_mat[st][obs] = float("-inf")
+                    continue
+
+                max_val = float("-inf")
                 max_state = None
                 for prev_st in xrange(num_states):
-                    val = dp_mat[prev_st][obs-1] + math.log(trans_distr[prev_st][st])
+                    val = (dp_mat[prev_st][obs-1] +
+                           math.log(self.trans_table[prev_st][st]))
                     if val > max_val:
                         max_val = val
                         max_state = prev_st
 
                 dp_mat[st][obs] = (max_val +
-                                   math.log(emission_prob(st, observ_seq[obs])))
+                            math.log(self.emission_prob(st, observ_seq[obs])))
                 backtrack[st][obs] = max_state
 
         #final state
-        final_score = -float(sys.maxint)
+        final_score = float("-inf")
         final_state = None
-        for st in xrange(num_states):
+        for st in self.ending_states:
             if dp_mat[st][-1] > final_score:
                 final_score = dp_mat[st][-1]
                 final_state = st
@@ -160,7 +185,13 @@ class NanoHMM(object):
             states.append(st)
 
         states = states[::-1]
-        return states
+        return states, self._decode_states(states)
+
+    def _decode_states(self, states):
+        kmers = map(self.id_to_state.get, states)
+        weights = map(lambda x: x[0], kmers)
+        weights.extend(kmers[-1][1:])
+        return "".join(weights[self.window - 1 : -self.window + 1])
 
 
 def _normalize(signal):
@@ -172,6 +203,13 @@ AA_SIZE_TRANS = maketrans("GASCTDPNVEQHLIMKRFYW-",
                           "LLLLLLLMMMMMMMHHHHHH-")
 def _aa_to_weights(kmer):
     return kmer.translate(AA_SIZE_TRANS)
+
+
+def _hamming_dist(str_1, str_2):
+    res = 0
+    for a, b in zip(str_1, str_2):
+        res += int(a != b)
+    return res
 
 
 def _avg_volume(kmer):
@@ -188,8 +226,16 @@ def main():
                             nano_hmm.reverse)
 
     discrete_events = nano_hmm.learn_emissions_distr(averages)
+    correct_weights = _aa_to_weights(nano_hmm.peptide)
+    print(correct_weights)
+    print()
     for obs in discrete_events:
-        nano_hmm.hmm(obs)
+        states, weights = nano_hmm.hmm(obs)
+
+        accuracy = _hamming_dist(weights, correct_weights)
+        accuracy = (float(len(correct_weights)) - accuracy) / len(correct_weights)
+        print(weights, accuracy)
+        nano_hmm.model_fit(states)
 
 
 if __name__ == "__main__":
